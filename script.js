a5b2a88e45b98947de474afb82bac3a698af353b
Gson <gtayone11@gmail.com>
2023-08-22 10:37:12 +0800

    Created a global variable for acctbalance & number

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const loginForm = document.getElementById("login-form");
-
-//   loginForm.addEventListener("submit", function (event) {
-//     event.preventDefault(); // Prevent form submission
-
-//     const username = document.getElementById("username-input").value;
-//     const password = document.getElementById("password-input").value;
-
-//     const adminUsername = "Gson";
-//     const adminPassword = "iamtheadmin";
-
-//     if (username === adminUsername && password === adminPassword) {
-//       window.location.href = "dashboard.html";
-//     } else {
-//       alert("Invalid credentials. Please try again.");
-//     }
-//   });
-// });
-
 const loginForm = document.getElementById("login-form");
 
+// Global variables
 const adminUsername = "Group5";
 const adminPassword = "iamtheadmin";
+const getAdminName = document.getElementById("adminName");
+
+// Account 1 variables
+let accountOneBalance = 8700;
+const accountOneNumber = "00014448332";
+
+// Account 2 variables
+let accountTwoBalance = 958;
+const accountTwoNumber = "00023578992";
 
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent form submission
 
   const username = document.getElementById("username-input").value;
   const password = document.getElementById("password-input").value;
-
-  const getAdminName = document.getElementById("adminName");
 
   if (username === adminUsername && password === adminPassword) {
     window.location.href = "dashboard.html";
